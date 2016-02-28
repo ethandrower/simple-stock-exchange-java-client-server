c206ba9a2e19b5ceb957e76a2f20454af6b65fc5
Ethan Drower <ethandrower@gmail.com>
2016-02-28 17:00:55 -0600

    Adding client exec code and feed loops

--- src/Client.java
+++ src/Client.java
@@ -3,10 +3,11 @@ import java.util.Queue;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Client implements Runnable{
+public class Client {
 	
 	// This class represents the initial LOCAL running client.
 	
@@ -68,12 +69,87 @@ public class Client implements Runnable{
 		
 	}
 	
-	public static void startExec(){
+	public static void startExec(String clientID, String connType) throws IOException{
 		
 		//read args from user input.
 		//open socket connection to server.
-		// send initial message to server to identify connection type.  array[clientID, Type]
+		Socket connExchange = new Socket("localhost", 123);
+		BufferedReader input = new BufferedReader(new InputStreamReader(connExchange.getInputStream()));
+		PrintWriter output = new PrintWriter(connExchange.getOutputStream());
 		
+		
+		// send initial message to server to identify connection type.  array[clientID, Type]
+		String initExec = clientID + "|" +  "EXEC";
+		output.println(initExec);
+		
+		boolean isRunning = true;
+		//Loop asking for user input, sending orders etc.    
+		
+		
+		
+		while(isRunning)
+		{
+			printMenu();
+			Console c = System.console();
+			String command = c.readLine();
+			int action = Integer.parseInt(command);
+			switch(action){
+			case 1:
+				//creat order
+				break;
+				
+			case 2: 
+				//cancel order
+				break;
+				
+			case 3:
+				//request market data
+				break;
+			case 4:
+				//exit
+				isRunning = false;
+				break;
+				
+			default:
+				System.out.println("Not a valid option");
+				break;
+			
+			}
+			
+			
+		}
+		
+		
+	}
+	public static void printMenu(){
+		System.out.println("You are connected to the exchange as an EXEC Feed:");
+		System.out.println("1 : Create Order" );
+		System.out.println("2 : Cancel Order" );
+		System.out.println("3 : Request Market Data");
+		System.out.println("4 : Quit");
+		
+		
+	}
+	
+	public static void startFeed(String clientID, int port) throws IOException{
+
+		Socket connFeedExchange = new Socket("localhost", 123);
+		BufferedReader input = new BufferedReader(new InputStreamReader(connFeedExchange.getInputStream()));
+		PrintWriter output = new PrintWriter(connFeedExchange.getOutputStream());
+		
+		
+		// send initial message to server to identify connection type.  array[clientID, Type]
+		String initExec = clientID + "|" +  "FEED";
+		output.println(initExec);
+		
+		boolean isRunning = true;
+		
+		while(isRunning)
+		{
+			String feed = input.readLine();
+			System.out.println("Message from gateway received!");
+			System.out.println(feed);
+		}
 		
 	}
 	
